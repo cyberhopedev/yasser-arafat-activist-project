23b051cf5f9bc93a8918c343e39fdc17afc5dd19
hkspence <hkspence@ncsu.edu>
2026-05-01 18:08:12 -0400

    [Implementation] Added const formatting for where to place research content within content.js

--- src/data/content.js
+++ src/data/content.js
@@ -6,34 +6,187 @@
 /**
 * Quick facts
 */
-export const quickFacts = {}
+export const quickFacts = {
+  Born: "",
+  Died: "",
+  Organization: "",
+  Role: "",
+  Region: "",
+}
 
 /**
 * Timeline of important events during Arafat's activism
 */
-export const timeline = []
+export const timeline = [
+  {
+    year:"",
+    event:"",
+    significance:"",
+  },
+  {
+    year:"",
+    event:"",
+    significance:"",
+  },
+  {
+    year:"",
+    event:"",
+    significance:"",
+  },
+  {
+    year:"",
+    event:"",
+    significance:"",
+  },
+  {
+    year:"",
+    event:"",
+    significance:"",
+  },
+  {
+    year:"",
+    event:"",
+    significance:"",
+  },
+  {
+    year:"",
+    event:"",
+    significance:"",
+  },
+  {
+    year:"",
+    event:"",
+    significance:"",
+  },
+  {
+    year:"",
+    event:"",
+    significance:"",
+  },
+  {
+    year:"",
+    event:"",
+    significance:"",
+  },
+  {
+    year:"",
+    event:"",
+    significance:"",
+  },
+  {
+    year:"",
+    event:"",
+    significance:"",
+  }
+];
 
 /**
 * Arafat's tactics used to pursue change
 */
-export const tactics = []
+export const tactics = [
+  {
+    tactic:"",
+    period: "",
+    description: "",
+    result: "",
+  },
+    {
+    tactic:"",
+    period: "",
+    description: "",
+    result: "",
+  },
+    {
+    tactic:"",
+    period: "",
+    description: "",
+    result: "",
+  },
+    {
+    tactic:"",
+    period: "",
+    description: "",
+    result: "",
+  },
+    {
+    tactic:"",
+    period: "",
+    description: "",
+    result: "",
+  },
+    {
+    tactic:"",
+    period: "",
+    description: "",
+    result: "",
+  },
+];
 
 /**
 * Table to compare Arafat's activism for Palestine to other study cases
 */
-export const comparisons = []
+export const comparisons = [
+  { 
+    caseStudy: "", 
+    similarity: "", 
+    difference: ""
+  },
+  { 
+    caseStudy: "", 
+    similarity: "", 
+    difference: ""
+  },
+  { 
+    caseStudy: "", 
+    similarity: "", 
+    difference: ""
+  },
+];
 
 /**
 * Sources used
 */
-export const sources = []
+export const sources = [
+  {
+    id: 1,
+    type: "article",
+    citation: "",
+    key_argument: "",
+    used_in: "",
+  },
+  {
+    id: 2,
+    type: "article",
+    citation: "",
+    key_argument: "",
+    used_in: "",
+  },
+  {
+    id: 3,
+    type: "article",
+    citation: "",
+    key_argument: "",
+    used_in: "",
+  },
+  {
+    id: 4,
+    type: "article",
+    citation: "",
+    key_argument: "",
+    used_in: "",
+  }
+];
 
 /**
 * Navigation items for the sidebar
 */
-export const navLinks = []
+export const navLinks = [
+
+];
 
 /**
 * Desktop icons for the fake desktop background on the site
 */
-export const desktopIcons = []
+export const desktopIcons = [
+
+];
